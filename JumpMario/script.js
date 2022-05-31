47954f3d85a39263728022e56c61eba8b0279b68
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-05-31 17:29:36 -0300

    Update script.js

--- JumpMario/script.js
+++ JumpMario/script.js
@@ -1,8 +1,26 @@
 const Mario = document.querySelector('#mario');
+const pipe = document.querySelector('#pipe')
 
-window.addEventListener('keydown', (event) => {
-    if(event.code == 'Space') {
-        Mario.classList.add('marioJump')
-        setInterval(() => {Mario.classList.remove('marioJump')}, 2000)
+function jump() {
+    Mario.classList.add('jump')
+    setTimeout(() => {
+        Mario.classList.remove('jump')
+    }, 700)
+}
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    
+    if(pipePosition <= 101) {
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+    }
+    
+}, 10)
+
+
+window.addEventListener('keydown',(event) => {
+    if(event.code == 'Space' || event.code == 'ArrowUp') {
+        jump()
     }
 })
